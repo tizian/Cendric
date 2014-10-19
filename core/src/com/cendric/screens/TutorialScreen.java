254ddad20998ed112b16addc362ee1c68249a3a1
Tizian Zeltner <tizian.zeltner@gmail.com>
2014-10-19 17:37:32 +0200

    + Added BitmapFont with a small test in TutorialScreen

--- core/src/com/cendric/screens/TutorialScreen.java
+++ core/src/com/cendric/screens/TutorialScreen.java
@@ -3,8 +3,10 @@ package com.cendric.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.cendric.CendricGame;
 import com.cendric.Constants;
 import com.cendric.Resources;
@@ -14,11 +16,15 @@ public class TutorialScreen implements Screen {
 	private CendricGame game;
 	private Screen lastScreen;
 	private Texture tutorialTexture;
+	private BitmapFont font;
 	
 	public TutorialScreen(CendricGame game, Screen lastScreen) {
 		this.game = game;
 		this.lastScreen = lastScreen;
 		tutorialTexture = Resources.tutorialScreen;
+		font = new BitmapFont(Gdx.files.internal("font/font.fnt"), false);
+		font.setColor(Color.WHITE);
+		font.setScale(4);
 	}
 	
 	@Override
@@ -32,6 +38,9 @@ public class TutorialScreen implements Screen {
 		
 		game.batch.begin();
 		game.batch.draw(tutorialTexture, 0, 0);
+		
+		font.draw(game.batch, "BITMAP FONT TEST BLA BLA", 10, Constants.WINDOW_HEIGHT - 30);
+		
 		game.batch.end();
 		
 		if (Gdx.input.isKeyJustPressed(Input.Keys.H)) {
@@ -64,5 +73,6 @@ public class TutorialScreen implements Screen {
 
 	@Override
 	public void dispose() {
+		font.dispose();
 	}
 }
