f06d4c4dd4315ed1096a47d1d3579707c5c64448
Tizian Zeltner <tizian.zeltner@gmail.com>
2014-10-27 22:30:16 +0100

    C A S I N O    E D I T I O N    M E N U

--- core/src/com/cendric/screens/MainMenuScreen.java
+++ core/src/com/cendric/screens/MainMenuScreen.java
@@ -6,8 +6,10 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.BitmapFont.HAlignment;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.cendric.CendricGame;
 import com.cendric.Constants;
 
@@ -17,11 +19,14 @@ public class MainMenuScreen implements Screen {
 	
 	private Texture titleText;
 	private Texture titleCendric;
+	private Animation casino;
 	
 	private BitmapFont font;
 	private Color purple = new Color(114/255f, 87/255f, 126/255f, 1f);
 	
 	private int selection;
+	private float stateTime;
+	private final float ANIMATION_TIME = 0.3f;
 	
 	public MainMenuScreen(CendricGame game, int selection) {
 		this.game = game;
@@ -35,7 +40,16 @@ public class MainMenuScreen implements Screen {
 		font.setColor(Color.WHITE);
 		font.setScale(5);
 		
+		// Animation
+		Texture casino1 = new Texture(Gdx.files.internal("sprites/CasinoEdition1.png"));
+		Texture casino2 = new Texture(Gdx.files.internal("sprites/CasinoEdition2.png"));
+		TextureRegion[] casinoRegion = new TextureRegion[2];
+		casinoRegion[0] = new TextureRegion(casino1);
+		casinoRegion[1] = new TextureRegion(casino2);
+		casino = new Animation(ANIMATION_TIME, casinoRegion);
+		
 		this.selection = selection;
+		stateTime = 0.0f;
 	}
 
 	@Override
@@ -50,37 +64,47 @@ public class MainMenuScreen implements Screen {
         game.batch.begin();
         
         // Draw title text
-        game.batch.draw(titleText, 110, 375, 749, 315);
+//        game.batch.draw(titleText, 110, 375, 749, 315);
+        game.batch.draw(titleText, (1280-titleText.getWidth()*7)/2 - 80, 375, titleText.getWidth()*7, titleText.getHeight()*7);
+        
+        // Draw casino edition animation
+        stateTime += delta;
+        TextureRegion casinoFrame = casino.getKeyFrame(stateTime, true);
+        game.batch.draw(casinoFrame, (1280-casinoFrame.getRegionWidth()/4)/2, 320, casinoFrame.getRegionWidth()/4, casinoFrame.getRegionHeight()/4);
         
         // Draw Cendric
-        game.batch.draw(titleCendric, 860, 25);
+        game.batch.draw(titleCendric, 820, -50, titleCendric.getWidth()/3, titleCendric.getHeight()/3);
         
         // Text
         font.setScale(5);
         font.setColor(Color.WHITE);
         
-        font.draw(game.batch, "START GAME", 280, 390);
-        font.draw(game.batch, "CONTROLS", 360, 325);
-        font.draw(game.batch, "ABOUT", 480, 250);
-        font.draw(game.batch, "EXIT", 520, 180);
+//        float startHeight = 390;
+        float startHeight = 310;
+        float deltaHeight = 60;
+        
+        font.draw(game.batch, "START GAME", 280, startHeight);
+        font.draw(game.batch, "CONTROLS", 360, startHeight-deltaHeight);
+        font.draw(game.batch, "ABOUT", 480, startHeight-2*deltaHeight);
+        font.draw(game.batch, "EXIT", 520, startHeight-3*deltaHeight);
         
         font.setColor(purple);
         
         if (selection == 0) {
-        	font.draw(game.batch, "START GAME", 280, 390);
-        	font.draw(game.batch, "<", 700, 390);
+        	font.draw(game.batch, "START GAME", 280, startHeight);
+        	font.draw(game.batch, "<", 700, startHeight);
         }
         if (selection == 1) {
-        	font.draw(game.batch, "CONTROLS", 360, 325);
-        	font.draw(game.batch, "<", 700, 325);
+        	font.draw(game.batch, "CONTROLS", 360, startHeight-deltaHeight);
+        	font.draw(game.batch, "<", 700, startHeight-deltaHeight);
         }
         if (selection == 2) {
-        	font.draw(game.batch, "ABOUT", 480, 250);
-        	font.draw(game.batch, "<", 700, 250);
+        	font.draw(game.batch, "ABOUT", 480, startHeight-2*deltaHeight);
+        	font.draw(game.batch, "<", 700, startHeight-2*deltaHeight);
         }
         if (selection == 3) {
-        	font.draw(game.batch, "EXIT", 520, 180);
-        	font.draw(game.batch, "<", 700, 180);
+        	font.draw(game.batch, "EXIT", 520, startHeight-3*deltaHeight);
+        	font.draw(game.batch, "<", 700, startHeight-3*deltaHeight);
         }
         
         font.setScale(2.4f);
