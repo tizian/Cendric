6cbfba74246f6ec1eb8e386a0c3b0c06b3830e97
Tizian Zeltner <tizian.zeltner@gmail.com>
2014-11-01 19:42:52 +0100

    * Rotating spell test

--- core/src/com/cendric/ecs/systems/SpriteRenderSystem.java
+++ core/src/com/cendric/ecs/systems/SpriteRenderSystem.java
@@ -1,8 +1,10 @@
 package com.cendric.ecs.systems;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.cendric.ecs.Entity;
 import com.cendric.ecs.components.ComponentType;
+import com.cendric.ecs.components.MovementComponent;
 import com.cendric.ecs.components.PositionComponent;
 import com.cendric.ecs.components.TextureComponent;
 
@@ -12,8 +14,19 @@ public class SpriteRenderSystem extends RenderSystem {
 	protected void render(Entity entity, SpriteBatch spriteBatch, float stateTime) {
 		PositionComponent pos = (PositionComponent) entity.getComponent(ComponentType.Position);
 		TextureComponent tex = (TextureComponent) entity.getComponent(ComponentType.Texture);
+		
 		if (pos == null) return;
 		if (tex == null) return;
+		
+		if (entity.tag.equals("Spell")) {
+			MovementComponent mov = (MovementComponent) entity.getComponent(ComponentType.Movement);
+			if (mov == null) return;
+			Vector2 bla = new Vector2(mov.vx, mov.vy);
+			if (tex.texture != null) {
+				spriteBatch.draw(tex.texture, pos.x, pos.y, 0, 0, tex.texture.getRegionWidth(), tex.texture.getRegionHeight(), 1, 1, bla.angle());
+			}
+			return;
+		}
 		
 		if (tex.texture != null) {
 			spriteBatch.draw(tex.texture, pos.x, pos.y);
